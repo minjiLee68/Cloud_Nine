17a717e1fb49c970f507dd6396d6edeab72c7218
minjiLee68 <dlalswl2598@gmail.com>
2021-12-22 18:09:25 +0900

    .

--- app/src/main/java/com/sophia/project_minji/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sophia/project_minji/mypage/MyPageFragment.kt
@@ -8,13 +8,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import com.sophia.UserPageActivity
 import com.sophia.project_minji.ProfileSetUpActivity
 import com.sophia.project_minji.adapter.FollowUserAdapter
+import com.sophia.project_minji.adapter.PagerAdapter
 import com.sophia.project_minji.adapter.UsersAdapter
 import com.sophia.project_minji.databinding.MypageFragmentBinding
 import com.sophia.project_minji.entity.FollowDto
@@ -33,14 +36,15 @@ class MyPageFragment : Fragment() {
     private lateinit var firestore: FirebaseFirestore
     private lateinit var auth: FirebaseAuth
     private lateinit var uid: String
-    private lateinit var userAdapter: FollowUserAdapter
-    private val users: ArrayList<User> = ArrayList()
+    private lateinit var pagerAdapter: PagerAdapter
 
     private lateinit var preferenceManager: PreferenceManager
 
     private val viewModel by viewModels<FirebaseViewModel> {
         FirebaseViewModelFactory(requireContext().applicationContext)
     }
+
+    private lateinit var newFriendsFrag: NewFriendsFragment
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,11 +59,13 @@ class MyPageFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         init()
+        createFragment()
         loadUserDetails()
         setListener()
-        initRecyclerView()
-        getUserObserver()
         followers()
+        createFragment()
+        createViewPager()
+        settingTabLayout()
     }
 
     private fun init() {
@@ -69,27 +75,61 @@ class MyPageFragment : Fragment() {
         preferenceManager = PreferenceManager(requireContext())
     }
 
+    private fun createFragment() {
+        newFriendsFrag = NewFriendsFragment()
+    }
+
+    private fun createViewPager() {
+        pagerAdapter = PagerAdapter(childFragmentManager, lifecycle)
+        pagerAdapter.addFragment(newFriendsFrag)
+        binding.viewPager2.adapter = pagerAdapter
+        binding.viewPager2.isUserInputEnabled = false
+    }
+
+    private fun settingTabLayout() {
+        binding.tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position) {
+                    0 -> {
+                        binding.viewPager2.currentItem = 0
+                    }
+                    1 -> {
+                        binding.viewPager2.currentItem = 1
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+    }
+
     private fun setListener() {
         binding.profileSetting.setOnClickListener {
-            val intent =
-                Intent(requireContext().applicationContext, ProfileSetUpActivity::class.java)
+            val intent = Intent(requireContext().applicationContext, ProfileSetUpActivity::class.java)
             startActivity(intent)
         }
     }
 
-    private fun initRecyclerView() {
-        userAdapter = FollowUserAdapter(users)
-        binding.userRecyclerView.adapter = userAdapter
-        binding.userRecyclerView.visibility = View.VISIBLE
-        binding.userRecyclerView.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-    }
-
-    private fun getUserObserver() {
-        viewModel.getUsers(users).observe(viewLifecycleOwner, {
-            userAdapter.submitList(it)
-        })
-    }
+//    private fun initRecyclerView() {
+//        userAdapter = FollowUserAdapter(users)
+//        binding.userRecyclerView.adapter = userAdapter
+//        binding.userRecyclerView.visibility = View.VISIBLE
+//        binding.userRecyclerView.layoutManager =
+//            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+//    }
+//
+//    private fun getUserObserver() {
+//        viewModel.getUsers(users).observe(viewLifecycleOwner, {
+//            userAdapter.submitList(it)
+//        })
+//    }
 
     private fun loadUserDetails() {
         firestore.collection("Users").document(uid).get()
@@ -117,6 +157,8 @@ class MyPageFragment : Fragment() {
             }
         }
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
---
+++ app/src/main/java/com/sophia/project_minji/mypage/NewFriendsFragment.kt
@@ -0,0 +1,85 @@
+package com.sophia.project_minji.mypage
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.sophia.project_minji.ProfileSetUpActivity
+import com.sophia.project_minji.adapter.FollowUserAdapter
+import com.sophia.project_minji.databinding.MypageFragmentBinding
+import com.sophia.project_minji.databinding.NewFriendsFragmentBinding
+import com.sophia.project_minji.entity.FollowDto
+import com.sophia.project_minji.entity.User
+import com.sophia.project_minji.utillties.PreferenceManager
+import com.sophia.project_minji.viewmodel.FirebaseViewModel
+import com.sophia.project_minji.viewmodel.FirebaseViewModelFactory
+
+class NewFriendsFragment : Fragment() {
+
+    private var _binding: NewFriendsFragmentBinding? = null
+    val binding: NewFriendsFragmentBinding
+        get() = _binding!!
+
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+    private lateinit var uid: String
+    private lateinit var userAdapter: FollowUserAdapter
+    private val users: ArrayList<User> = ArrayList()
+
+    private lateinit var preferenceManager: PreferenceManager
+
+    private val viewModel by viewModels<FirebaseViewModel> {
+        FirebaseViewModelFactory(requireContext().applicationContext)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = NewFriendsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+        initRecyclerView()
+        getUserObserver()
+    }
+
+    private fun init() {
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        uid = auth.currentUser?.uid.toString()
+        preferenceManager = PreferenceManager(requireContext())
+    }
+
+    private fun initRecyclerView() {
+        userAdapter = FollowUserAdapter(users)
+        binding.userRecyclerView.adapter = userAdapter
+        binding.userRecyclerView.visibility = View.VISIBLE
+        binding.userRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+    }
+
+    private fun getUserObserver() {
+        viewModel.getUsers(users).observe(viewLifecycleOwner, {
+            userAdapter.submitList(it)
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
